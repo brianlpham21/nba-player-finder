513c918bc2060d402ece2d09ec56f93ec41997a4
Brian Pham <brianlpham21@gmail.com>
2021-05-13 12:55:45 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React, { useState } from 'react';
 import './App.css';
-import LogoutButton from './LogoutButton';
-import Profile from './Profile';
-import { useAuth0 } from '@auth0/auth0-react';
-import Home from './Home';
-import Demo from './Demo';
+// import LogoutButton from './LogoutButton';
+// import Profile from './Profile';
+// import { useAuth0 } from '@auth0/auth0-react';
+// import Home from './Home';
+// import Demo from './Demo';
 
 export default function App() {
 
-  const [view, setView] = useState('')
+  // const [view, setView] = useState('')
 
-  const changeView = (name) => {
-    setView({ name })
-  }
+  // const changeView = (name) => {
+  //   setView({ name })
+  // }
 
-  const { isLoading } = useAuth0();
-  if (isLoading) return <div>Loading...</div>
+  // const { isLoading } = useAuth0();
+  // if (isLoading) return <div>Loading...</div>
 
-  let page = null;
-  if (view.name === "demo") {
-    page = <Demo changeView={changeView} />
-  } else {
-    page = <Home changeView={changeView} />
-  }
+  // let page = null;
+  // if (view.name === "demo") {
+  //   page = <Demo changeView={changeView} />
+  // } else {
+  //   page = <Home changeView={changeView} />
+  // }
 
   return (
     <>
-      {page}
-      <Profile />
-      <LogoutButton />
+      hello
     </>
   )
 }
